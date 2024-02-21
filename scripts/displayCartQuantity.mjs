20e76ed46d16436b1a5681af577d741897378006
H-chai <143179912+H-chai@users.noreply.github.com>
2024-02-21 22:04:38 +0100

    display cart quantity in header both pc and sp

--- scripts/displayCartQuantity.mjs
+++ scripts/displayCartQuantity.mjs
@@ -1,17 +1,20 @@
 export function getTotalAmount() {
-  const quantityContainer = document.querySelector(".quantity-container");
-  const quantityText = document.createElement("p");
-  quantityText.classList.add("quantity-text");
-  quantityContainer.appendChild(quantityText);
-  
   const currentLocalStorageData = JSON.parse(localStorage.getItem("cart"));
-  
-  let totalAmount = 0;
-  for(let i = 0; i < currentLocalStorageData.length; i++) {
-    const itemAmountPerItem = currentLocalStorageData[i].amount;
-    totalAmount = totalAmount + itemAmountPerItem;
-  }
-  quantityText.textContent = totalAmount;
+
+  const quantityContainers = document.querySelectorAll(".quantity-container");
+
+  Array.from(quantityContainers).forEach(quantityContainer => {
+    const quantityText = document.createElement("p");
+    quantityText.classList.add("quantity-text");
+    quantityContainer.appendChild(quantityText);
+
+    let totalAmount = 0;
+    for(let i = 0; i < currentLocalStorageData.length; i++) {
+      const itemAmountPerItem = currentLocalStorageData[i].amount;
+      totalAmount = totalAmount + itemAmountPerItem;
+    }
+    quantityText.textContent = totalAmount;
+  });
 
 }
 
